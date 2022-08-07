40b912db299fd1b145baa5460a0733ceed0438f7
Cr-collab <cristiccorrea@gmail.com>
2022-08-07 14:31:32 -0300

    feat: rota de criação de categorias e rota de listagem de categorias.

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,1 +1,18 @@
-import {Router} from 'express'
+import { Router } from "express";
+
+export const categoriesRoutes = Router();
+
+const categories = [];
+
+categoriesRoutes.post("/categories", (request, response) => {
+  const { name, description } = request.body;
+
+  categories.push({ name, description });
+
+  return response.status(201).send();
+});
+
+
+categoriesRoutes.get("/categories", (request, response) => {
+    return response.json(categories);
+})
